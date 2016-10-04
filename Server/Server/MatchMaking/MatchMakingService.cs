58a8ac8be7f09fd3e4959aa6c3d32e9d1985f398
pjc02478 <pjc0247@gmail.com>
2016-10-04 14:13:13 +0900

    f

--- Server/Server/MatchMaking/MatchMakingService.cs
+++ Server/Server/MatchMaking/MatchMakingService.cs
@@ -67,7 +67,6 @@ namespace Server.MatchMaking
 
             currentPlayerId = p.senderId;
             matchMaker.Enqueue(this);
-            //matchMaker.Enqueue(ReservedPlayerId.Bot);
 
             clientState = ClientState.QueueJoined;
         }
---
+++ Server/Server/MatchMaking/QueueType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.MatchMaking
+{
+    enum QueueType
+    {
+        Elo_0_500,
+        Elo_501_1000,
+        Elo_1001_1500,
+        Elo_1501_2000,
+        Elo_2001_9999,
+
+        BotGame,
+    }
+}
