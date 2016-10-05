cbe1e33e7f7e046a8a74c60abb1b083e4ccf3aa2
pjc02478 <pjc0247@gmail.com>
2016-10-05 15:32:05 +0900

    LB prototype

---
+++ Server/Server/LoadBalancing/LoadBalancerFixedRound.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server.LoadBalancing
+{
+    class LoadBalancerFixedRound : ILoadBalancer
+    {
+        private string[] hosts { get; set; }
+
+        private uint cursor;
+
+        public void Initialize(string[] hosts)
+        {
+            if (hosts == null || hosts.Length == 0)
+                throw new ArgumentNullException(nameof(hosts));
+
+            this.hosts = hosts;
+        }
+
+        public string GetNext()
+        {
+            unchecked
+            {
+                cursor++;
+
+                return hosts[(cursor % hosts.Length)];
+            }
+        }
+    }
+}
---
+++ Server/Server/LoadBalancing/LoadBalancerFlexibleRound.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server.LoadBalancing
+{
+    class LoadBalancerFlexibleRound : ILoadBalancer
+    {
+        private ConcurrentQueue<string> hosts;
+
+        public void Initialize(string[] hosts)
+        {
+            if (hosts == null || hosts.Length == 0)
+                throw new ArgumentNullException(nameof(hosts));
+
+            foreach(var host in hosts)
+            {
+                this.hosts.Enqueue(host);
+            }
+        }
+
+        public string GetNext()
+        {
+            string host = null;
+
+            Retry:
+
+            if (hosts.TryDequeue(out host))
+            {
+                hosts.Enqueue(host);
+                return host;
+            }
+            else
+            {
+                Thread.SpinWait(10);
+                goto Retry;
+            }
+        }
+    }
+}
