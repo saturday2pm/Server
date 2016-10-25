d06ac889440ab10328243a808475894eec24142e
Park June Chul <pjc0247@naver.com>
2016-10-25 23:42:38 +0900

    Auth.Facebook 프로토타입

---
+++ Server/Server/Auth/IDP/IDProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Auth.IDP
+{
+    interface IDProvider
+    {
+        Task<bool> IsValidToken(string userId, string accessToken);
+    }
+}
