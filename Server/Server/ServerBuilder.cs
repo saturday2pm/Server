9ec9beeff14dcee0c8b11a56566ad532f60151dc
pmnxis <pmnxis@gmail.com>
2016-11-29 11:52:03 +0900

    개별 인스턴스 빌드하는 서브프로제트 추가

---
+++ Server/Server/ServerBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using GSF;
+
+namespace Server
+{
+    using Env;
+
+    public class ServerBuilder
+    {
+        private GSF.Server server { get; set; }
+
+        public static ServerBuilder Create()
+        {
+            return new ServerBuilder();
+        }
+
+        public ServerBuilder WithService(ServiceNames name)
+        {
+            switch(name)
+            {
+                case ServiceNames.Ingame:
+                    server.WithService<Ingame.IngameService>(Ingame.IngameService.Path);
+                    break;
+                case ServiceNames.Matchmaking:
+                    server.WithService<MatchMaking.MatchMakingService>(MatchMaking.MatchMakingService.Path);
+                    break;
+            }
+
+            return this;
+        }
+
+        public void Start()
+        {
+            var ev = ServerEnv.selectedEnv;
+
+            server = GSF.Server.Create(ev.port);
+            server.Run();
+
+            while (true)
+            {
+                var ch = Console.Read();
+
+                if (ch == 'q') break;
+            }
+        }
+    }
+}
---
+++ Server/instance_game/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Server
+{
+    using Env;
+    using MatchMaking;
+    using Ingame;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerBuilder.Create()
+                .WithService(ServiceNames.Ingame)
+                .Start();
+        }
+    }
+}
